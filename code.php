399953dedbaa114c1f9abc3a8de0a051041a0dad
msledesma <100432228+msledesma@users.noreply.github.com>
2022-07-08 19:51:29 -0300

    Create code.php

---
+++ code.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Web Guapas</title>
+        <link rel="stylesheet" href="guapas_estilos.css">
+    </head>
+    <body>
+       <?php
+       $to="lsmariasol03@gmail.com";
+        
+       $email_subject="Mail desde web guapas"
+        
+       $email_body="Te escribio: " .$nombre "Con email: " .$email "Consulta: " .$texto
+        
+        echo='Pronto nos comunicaremos con usted!'?>
+      </p>        
+    </body>
+</html>
